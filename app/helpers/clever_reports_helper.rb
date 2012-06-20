708c4240ef5793717095f086b1f93157716fa095
Ian Mooney <ian@yoomee.com>
2012-06-20 10:09:05 +0100

    Fix options_for_clever_field_names for tag_list

--- app/helpers/clever_reports_helper.rb
+++ app/helpers/clever_reports_helper.rb
@@ -55,10 +55,12 @@ module CleverReportsHelper
     klass.clever_fields.inject("") do |out, field_name|
       if klass.custom_clever_options.keys.collect(&:to_s).include?(field_name.to_s)
         column_type = "custom_select"
+      elsif column = klass.columns.detect{|col| col.name == field_name}
+        column_type = column.type.to_s
       elsif field_name == "tag_list"
-        column_type = "tag_list"
+        column_type = "tag_list"        
       else
-        column_type = klass.columns.detect{|col| col.name == field_name}.type.to_s
+        column_type = "string"
       end
       tag_options = {:class => "clever_type_#{column_type}", :value => field_name}
       tag_options[:selected] = "selected" if options[:selected] == field_name
